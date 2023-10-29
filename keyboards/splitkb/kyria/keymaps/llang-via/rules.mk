f59967b462fcbc6ec7a303c8f1c0dd45a09e696f
Lutz Lang <llang@me.com>
2023-10-29 17:02:26 +0100

    Configured for VIA and Liatris Controller, removed non-QWERTY Keymaps, added personal name to display, added Clipboard Copy/Paste to NAV layer.

---
+++ keyboards/splitkb/kyria/keymaps/llang-via/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+VIA_ENABLE = yes
+NKRO_ENABLE = yes
+KEY_LOCK_ENABLE = no
+SPACE_CADET_ENABLE = no
+MAGIC_ENABLE = no
+GRAVE_ESCAPE_ENABLE = no
+ENCODER_MAP_ENABLE = yes
+
+CONVERT_TO=liatris
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = no       # Disable keyboard RGB underglow
